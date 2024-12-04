65e9dcd9cb63429f8d4ad216c438789bfd0d6adb
Klimentin0 <meshkov.klim@gmail.com>
2024-12-04 15:56:21 +0300

    просто тренировочка

---
+++ src/views/AboutView.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { useFlash } from '@/composables/useFlash'
+let { flash } = useFlash()
+</script>
+
+<template>
+  <div>
+    <h1>This is an about page</h1>
+
+    <button @click="flash('working on the about page')">test</button>
+  </div>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,36 @@
+<script setup>
+import { onMounted, ref } from 'vue';
+
+let textarea = ref(null);
+
+
+onMounted(() => {
+  textarea.value.addEventListener("keydown", (e) => {
+    let t = textarea.value;
+
+    //tab is pressed
+    if (e.keycode ==9 ) {
+      //get caret position/selection
+      let val = textarea.value,
+      start = textarea.value.selectionStart,
+      end = textarea.value.selectionEnd;
+
+      // set textarea value to: text vefore caret + tab + text after caret
+      textarea.value.value = val.substring(0, start) + "\t" + val.substring(end);
+
+      // put caret at right position again
+      textarea.value.selectionStart = textarea.value.selectionEnd = start + 1;
+
+      e.preventDefault();
+    }
+  })
+})
+</script>
+
+<template>
+  <main>
+    <form>
+      <textarea ref="" style="width: 100%; height: 300px;">testarea</textarea>
+    </form>
+  </main>
+</template>
